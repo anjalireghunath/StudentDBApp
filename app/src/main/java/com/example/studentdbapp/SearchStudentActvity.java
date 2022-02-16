5cbfb658be3b577795fe1927131829c247479d4a
sahinasahee <sahinasahee>
2022-02-16 10:01:42 +0530

    Add Stud and Serach stud Activity Completed

--- app/src/main/java/com/example/studentdbapp/SearchStudentActvity.java
+++ app/src/main/java/com/example/studentdbapp/SearchStudentActvity.java
@@ -1,14 +1,41 @@
 package com.example.studentdbapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SearchStudentActvity extends AppCompatActivity {
-
+EditText ed1,ed2,ed3,ed4;
+AppCompatButton b1,b2;
+String getAdmission;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_student_actvity);
+        ed1=(EditText) findViewById(R.id.admision);
+        ed2=(EditText) findViewById(R.id.name);
+        ed3=(EditText) findViewById(R.id.roll);
+        ed4=(EditText) findViewById(R.id.colege);
+        b1=(AppCompatButton) findViewById(R.id.search);
+        b2=(AppCompatButton) findViewById(R.id.backtom);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getAdmission=ed1.getText().toString();
+                Toast.makeText(getApplicationContext(),getAdmission, Toast.LENGTH_SHORT).show();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
